eae7ad545a29c0377313455544e26b70f75731c8
Sasha Lorion <sasha.lorion@gmail.com>
2025-08-22 17:37:24 +0200

    Enhance 'creet' type by adding mutable properties for color, state, and interaction. This update introduces attributes for is_dead, is_infected, has_bounced, berserk, mean, and is_dragging to improve creature behavior and user interaction.

--- bin/types.ml
+++ bin/types.ml
@@ -3,5 +3,12 @@ type creet = {
   mutable y: float;
   mutable vx: float;
   mutable vy: float;
-  radius: float;
+  mutable radius: float;
+  mutable color: string;
+  mutable is_dead: bool;
+  mutable is_infected: bool;
+  mutable has_bounced: bool;
+  mutable berserk: bool;
+  mutable mean: bool;
+  mutable is_dragging: bool;
 }
