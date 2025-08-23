3204d34b765577bd25c05528be30c9d849d2d776
Sasha Lorion <sasha.lorion@gmail.com>
2025-08-23 12:47:56 +0200

    Add display functions for game state in draw.ml
    - Introduced display_total_creet to show the total number of 'creet' on the canvas.
    - Added display_game_over function to handle game over scenarios, displaying appropriate messages based on the state of 'creet'.
    - Enhanced visual feedback for game over conditions with clear canvas and styled text.

--- bin/draw.ml
+++ bin/draw.ml
@@ -23,3 +23,37 @@ let draw_a_creet context color creet =
   context##.fillStyle := Js.string color;
   context##arc creet.x creet.y creet.radius 0. (2. *. Float.pi) (Js.bool false); (* x, y, radius, startAngle, endAngle, counterclockwise *)
   context##fill
+
+let display_total_creet ctxt creet =
+  ctxt##.fillStyle := Js.string "black";
+  ctxt##.font := Js.string "16px Arial serif";
+  ctxt##fillText (Js.string (Printf.sprintf "Total: %d" (List.length creet))) 10. 30.
+
+let display_game_over ctx living_creets healthy_creets height width game_over =
+   if List.length healthy_creets = 0 && List.length living_creets > 0 then begin
+    (* Toutes les créatures vivantes sont malades - GAME OVER *)
+    ctx##clearRect 0. 0. (width *. 2.) height;
+    ctx##.fillStyle := Js.string "rgb(0, 0, 0)";
+    ctx##fillRect 0. 0. (width *. 2.) height;
+    
+    ctx##.fillStyle := Js.string "red";
+    ctx##.font := Js.string "48px Arial";
+    ctx##.textAlign := Js.string "center";
+    ctx##fillText (Js.string "GAME OVER") width (height /. 4.);
+    game_over := true;
+    ()
+  end
+  else if List.length living_creets = 0 then begin
+    (* Toutes les créatures sont mortes - GAME OVER aussi *)
+    ctx##clearRect 0. 0. (width *. 2.) height;
+    ctx##.fillStyle := Js.string "rgb(0, 0, 0)";
+    ctx##fillRect 0. 0. (width *. 2.) height;
+
+    
+    ctx##.fillStyle := Js.string "red";
+    ctx##.font := Js.string "48px Arial";
+    ctx##.textAlign := Js.string "center";
+    ctx##fillText (Js.string "GAME OVER") width (height /. 4.);
+    game_over := true;
+    ()
+  end
